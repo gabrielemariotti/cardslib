74922e278ec96c709f6975b6ed3a70441411e6a3
gabrielemariotti <gabri.mariotti@gmail.com>
2014-08-06 00:09:45 +0200

    Demo: changed the style for materialDesign

---
+++ demo/stock/src/main/java/it/gmariotti/cardslib/demo/fragment/androidL/BaseMaterialFragment.java
@@ -0,0 +1,80 @@
+/*
+ * ******************************************************************************
+ *   Copyright (c) 2013-2014 Gabriele Mariotti.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *  *****************************************************************************
+ */
+
+package it.gmariotti.cardslib.demo.fragment.androidL;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import it.gmariotti.cardslib.demo.R;
+import it.gmariotti.cardslib.demo.fragment.BaseFragment;
+import it.gmariotti.cardslib.demo.utils.LPreviewUtils;
+import it.gmariotti.cardslib.demo.utils.LPreviewUtilsBase;
+
+/**
+ * @author Gabriele Mariotti (gabri.mariotti@gmail.com)
+ */
+public abstract class BaseMaterialFragment extends BaseFragment {
+
+    private View mHeaderBox;
+    private View mHeaderContentBox;
+    private View mHeaderBackgroundBox;
+    private View mHeaderShadow;
+
+    TextView mTitleHeader;
+    TextView mSubTitleHeader;
+    
+    @Override
+    public int getTitleResourceId() {
+        return R.string.app_name;
+    }
+
+
+    protected void setupHeader(View rootView){
+        mHeaderBox = rootView.findViewById(R.id.header_recap);
+        mHeaderContentBox = rootView.findViewById(R.id.header_contents);
+        mHeaderBackgroundBox = rootView.findViewById(R.id.header_background);
+        mHeaderShadow = rootView.findViewById(R.id.header_shadow);
+        
+        mTitleHeader = (TextView) rootView.findViewById(R.id.header_title);
+        mSubTitleHeader = (TextView) rootView.findViewById(R.id.header_subtitle);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        LPreviewUtilsBase lpu = LPreviewUtils.getInstance(getActivity());
+
+        float mMaxHeaderElevation = getResources().getDimensionPixelSize(
+                R.dimen.session_detail_max_header_elevation);
+
+        mHeaderShadow.setVisibility(lpu.hasLPreviewAPIs() ? View.GONE : View.VISIBLE);
+        lpu.setViewElevation(mHeaderBackgroundBox,  mMaxHeaderElevation);
+        lpu.setViewElevation(mHeaderContentBox,  mMaxHeaderElevation + 0.1f);
+    
+        mTitleHeader.setText(getString(getTitleHeaderResourceId()));
+        mSubTitleHeader.setText(getString(getSubTitleHeaderResourceId()));
+
+    }
+
+    protected abstract int getSubTitleHeaderResourceId();
+
+    protected abstract int getTitleHeaderResourceId();  
+}
--- demo/stock/src/main/java/it/gmariotti/cardslib/demo/fragment/androidL/NativeCardFragment.java
+++ demo/stock/src/main/java/it/gmariotti/cardslib/demo/fragment/androidL/NativeCardFragment.java
@@ -26,9 +26,6 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import it.gmariotti.cardslib.demo.R;
-import it.gmariotti.cardslib.demo.fragment.BaseFragment;
-import it.gmariotti.cardslib.demo.utils.LPreviewUtils;
-import it.gmariotti.cardslib.demo.utils.LPreviewUtilsBase;
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardHeader;
 import it.gmariotti.cardslib.library.internal.base.BaseCard;
@@ -37,46 +34,29 @@ import it.gmariotti.cardslib.library.view.CardViewNative;
 /**
  * @author Gabriele Mariotti (gabri.mariotti@gmail.com)
  */
-public class NativeCardFragment extends BaseFragment{
-
-    private View mHeaderBox;
-    private View mHeaderContentBox;
-    private View mHeaderBackgroundBox;
-    private View mHeaderShadow;
-
-    @Override
-    public int getTitleResourceId() {
-        return R.string.app_name;
-    }
+public class NativeCardFragment extends BaseMaterialFragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View mRootView = inflater.inflate(R.layout.demo_fragment_native_card, container, false);
-
-        mHeaderBox = mRootView.findViewById(R.id.header_session);
-        mHeaderContentBox = mRootView.findViewById(R.id.header_contents);
-        mHeaderBackgroundBox = mRootView.findViewById(R.id.header_background);
-        mHeaderShadow = mRootView.findViewById(R.id.header_shadow);
-
-
-
+        setupHeader(mRootView);
         return mRootView;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        LPreviewUtilsBase lpu = LPreviewUtils.getInstance(getActivity());
-
-        float mMaxHeaderElevation = getResources().getDimensionPixelSize(
-                R.dimen.session_detail_max_header_elevation);
-
-        mHeaderShadow.setVisibility(lpu.hasLPreviewAPIs() ? View.GONE : View.VISIBLE);
-        lpu.setViewElevation(mHeaderBackgroundBox,  mMaxHeaderElevation);
-        lpu.setViewElevation(mHeaderContentBox,  mMaxHeaderElevation + 0.1f);
-
         initCards();
+    }
+
+    @Override
+    protected int getSubTitleHeaderResourceId() {
+        return R.string.header_title_bigcard;
+    }
+
+    @Override
+    protected int getTitleHeaderResourceId() {
+        return R.string.header_title_examples;
     }
 
     private void initCards() {
