17388cc21a7ec2c5b78c21984a748f3df6af1b33
gabrielemariotti <gabri.mariotti@gmail.com>
2014-11-12 09:11:51 +0100

    Lib-core: changed the code for shadow

--- library-core/src/main/java/it/gmariotti/cardslib/library/view/CardViewNative.java
+++ library-core/src/main/java/it/gmariotti/cardslib/library/view/CardViewNative.java
@@ -30,7 +30,6 @@ import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.LayoutRes;
-import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -291,6 +290,7 @@ public class CardViewNative extends android.support.v7.widget.CardView implement
 
     }
 
+
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void drawableHotspotChanged(float x, float y) {
@@ -421,10 +421,7 @@ public class CardViewNative extends android.support.v7.widget.CardView implement
      */
     protected void setupShadowView() {
         if (mCard != null && mCard.getCardElevation() != null) {
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
-                ViewCompat.setElevation(this,mCard.getCardElevation());
-            else
-                ViewCompat.setElevation(this,mCard.getCardElevation());
+            this.setCardElevation(mCard.getCardElevation());
         }
     }
 
